02e4d11eb3be537f761b514d5fc1877f2672fe11
JR Oakes <jroakes@gmail.com>
2022-09-19 10:47:52 -0400

    Update __init__.py

--- src/googlewrapper/__init__.py
+++ src/googlewrapper/__init__.py
@@ -1,6 +1,6 @@
 """General API Connector Classes for Google Products"""
 
-
+from .cal import GoogleCalendar
 from .ga import GoogleAnalytics
 from .gsc import GoogleSearchConsole
 from .gbq import GoogleBigQuery
