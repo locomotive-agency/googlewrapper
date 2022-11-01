c6ef0b0d0721a316bdc5a50e74cef1a6045136c2
Savannah Casto <89162068+SavannahCasto@users.noreply.github.com>
2022-11-01 10:28:19 -0400

    Update authentication
    Replace depreciated oauth method with service account

--- src/googlewrapper/connect.py
+++ src/googlewrapper/connect.py
@@ -32,8 +32,7 @@ class Connection:
     ** requires a separate service account authentication file **
     """
 
-    def __init__(self, file_path="client_secret.json") -> None:
-        self.file_path = file_path
+    def __init__(self) -> None:
         self.__dir_check()
 
     def __dir_check(self) -> None:
@@ -41,108 +40,67 @@ class Connection:
         if not Path("./credentials/").is_dir():
             Path("./credentials/").mkdir()
 
-    def _authenticate(self, scope: list, token_name: str, http_return: bool = True):
-        """
-        Authenticates Google Product using oauth2
-        Saves the credentials in the ./credentials folder
-        Returns the authroized resource object
+    def _authenticate(self, scope: list, service_account_key: str, service_account_subject: str):
+         return service_account.Credentials.from_service_account_file(service_account_key,
+                                                                 scopes=scope,
+                                                                 subject=service_account_subject)
 
-        Params:
-        scope: list of strings (urls from google)
-            to find list of all scopes
-                https://developers.google.com/identity/protocols/oauth2/scopes
-
-        token_name: string: what we will name our crednetials.dat file
-
-        Method is based on Google Analytics Authentication found here:
-        https://developers.google.com/analytics/devguides/
-            reporting/core/v4/quickstart/installed-py#3_setup_the_sample
-        """
-        # Parse command-line arguments
-        # this helps determine if we are in a notebook or not
-        parser = argparse.ArgumentParser(
-            formatter_class=argparse.RawDescriptionHelpFormatter,
-            parents=[tools.argparser],
-        )
-        flags = parser.parse_args([])
-
-        # if in a notebook, set noauth to True
-        # this allows authentication to work on notebooks
-        if parser.prog == "ipykernel_launcher.py":
-            flags.noauth_local_webserver = True
-
-        # Set up a Flow object to be used if we need to authenticate.
-        flow = client.flow_from_clientsecrets(
-            self.file_path,
-            scope=scope,
-            message=tools.message_if_missing(self.file_path),
-        )
-
-        # Prepare credentials, and authorize HTTP object with them.
-        # If the credentials don't exist or are invalid
-        # run through the native client flow.
-        # The Storage object will ensure that if successful the good
-        # credentials will get written back to a file.
-        storage = file.Storage(f"./credentials/{token_name}.dat")
-        credentials = storage.get()
-
-        if credentials is None or credentials.invalid:
-            credentials = tools.run_flow(flow, storage, flags)
-
-        if http_return:
-            return credentials.authorize(http=httplib2.Http())
-
-        return credentials
-
-    def gsc(self):
+    def gsc(self, service_account_key, service_account_subject):
         """Google Search Console Connection Method"""
         scope_list = ["https://www.googleapis.com/auth/webmasters.readonly"]
         return build(
-            "searchconsole", "v1", http=self._authenticate(scope_list, "search_console")
+            "searchconsole", "v1", credentials=self._authenticate(scope_list, service_account_key, service_account_subject)
         )
 
-    def ga(self):
+    def ga(self, service_account_key, service_account_subject):
         """Google Analytics Connection Method"""
         scope_list = ["https://www.googleapis.com/auth/analytics.readonly"]
         return build(
-            "analyticsreporting", "v4", http=self._authenticate(scope_list, "analytics")
+            "analyticsreporting", "v4", credentials=self._authenticate(scope_list, service_account_key, service_account_subject)
         )
 
-    def cal(self):
+    def cal(self, service_account_key, service_account_subject):
         """Google Calendar Connection Method"""
         scope_list = ["https://www.googleapis.com/auth/calendar"]
-        return build("calendar", "v3", http=self._authenticate(scope_list, "calendar"))
+        return build(
+            "calendar", "v3", credentials=self._authenticate(scope_list, service_account_key, service_account_subject)
+        )
 
     def pygsheets(self):
         "Pygsheets Connection Method"
         return pygsheets.authorize(local=True, credentials_directory="./credentials/")
 
-    def sheets(self):
+    def sheets(self, service_account_key, service_account_subject):
         """Google Sheets Connection Method"""
         scope_list = [
             "https://www.googleapis.com/auth/drive",
             "https://www.googleapis.com/auth/spreadsheets",
         ]
-        return build("sheets", "v4", http=self._authenticate(scope_list, "sheets"))
+        return build(
+            "sheets", "v4", credentials=self._authenticate(scope_list, service_account_key, service_account_subject)
+        )
 
-    def gbq(self, sa_file_path="gbq-sa.json"):
+    def gbq(self, service_account_key, service_account_subject):
         """
         Google Big Query Connection Method
         Requires a different .json file to connect
         """
         return service_account.Credentials.from_service_account_file(sa_file_path)
 
-    def gmail(self):
+    def gmail(self, service_account_key, service_account_subject):
         """GMAIL Connection Method"""
         scope_list = ["https://mail.google.com/"]
-        return build("gmail", "v1", http=self._authenticate(scope_list, "gmail"))
+        return build("gmail", "v1", credentials=self._authenticate(scope_list, service_account_key, service_account_subject)
+    )
 
-    def drive(self):
+    def drive(self, service_account_key, service_account_subject):
         """Google Drive Connection Method"""
         scope_list = ["https://www.googleapis.com/auth/drive.readonly"]
-        return build("drive", "v3", http=self._authenticate(scope_list, "drive"))
+        return build("drive", "v3", credentials=self._authenticate(scope_list, service_account_key, service_account_subject)
+    )
 
-    def docs(self):
+    def docs(self, service_account_key, service_account_subject):
         """Google Docs Connection Method"""
         scope_list = ["https://www.googleapis.com/auth/drive.readonly"]
-        return build("docs", "v1", http=self._authenticate(scope_list, "docs"))
+        return build("docs", "v1", credentials=self._authenticate(scope_list, service_account_key, service_account_subject)
+    )
